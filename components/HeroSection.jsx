5c199e27a5a3b6ec8a9865b7ce1549ffbf66c23e
Asadali-rajpoot <alirjpot8@gmail.com>
2025-02-13 21:17:10 +0500

    First commit

---
+++ components/HeroSection.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+export default function HeroSection() {
+  return (
+    <div className="h-screen w-screen bg-[url('/images/logo.png')]">
+      <Navbar />
+    </div>
+  )
+}
---
+++ components/Navbar.jsx
@@ -0,0 +1,22 @@
+import { Button } from "./ui/button";
+import logo from "@/public/images/logo.png"
+import Image from "next/image";
+
+export default function Navbar(props) {
+  return (
+    <div className="flex justify-between bg-hero-bg bg-cover bg-black py-4 px-5">
+      {/* logo */}
+      <Image src={logo} alt="logo" className="h-8" quality={100} />
+      {/* nav menu */}
+      <ul className="flex items-center gap-5 *:font-RubikFont font-medium text-[#9e9e9e]">
+        {/* individual items */}
+        <li>Home</li>
+        <li>About</li>
+        <li>Services</li>
+        <li>Contact</li>
+      </ul>
+      {/* join now button */}
+      <Button className="bg-white text-[#212121] font-RubikFont font-semibold text-sm hover:bg-white/80">Join now</Button>
+    </div>
+  )
+}
